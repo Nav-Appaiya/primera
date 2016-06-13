4bd812783351c1645f6d8118b6443ae73e9f2c1c
Nav Appaiya <navarajh@gmail.com>
2016-06-13 19:40:04 +0200

    HTML raw output in page template & added some seeds for pages

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,8 @@ class DatabaseSeeder extends Seeder
         $productImage = 'http://www.esigaretervaring.nl/wp-content/uploads/2014/04/Elektrische-sigaret-voorbeeld-300x225.jpg';
         $createdAt = new DateTime();
         $updatedAt = new DateTime();
+
+        // Users seeder
         DB::table('users')->insert([
             'nickname' => 'admin',
             'email' => 'navarajh'.mt_rand(0,200).'@gmail.com',
@@ -32,6 +34,7 @@ class DatabaseSeeder extends Seeder
             'updated_at' => $updatedAt
         ]);
 
+        // Orders seeder
         DB::table('orders')->insert([
             'user_id' => mt_rand(0,1200),
             'shipping_address' => 'rijnvoorde 42, 3085TJ Rotterdam',
@@ -42,6 +45,7 @@ class DatabaseSeeder extends Seeder
             'updated_at' => $updatedAt
         ]);
 
+        // Product seeder
         DB::table('products')->insert([
             'name' => 'product ' . mt_rand(0, 200),
             'description' => 'Beschrijving bij product',
@@ -52,10 +56,35 @@ class DatabaseSeeder extends Seeder
             'updated_at' => $updatedAt
         ]);
 
+        // Category seeder
         DB::table('categories')->insert([
             'categoryID'=>'1',
             'title'=>'Category' . mt_rand(0,15),
             'image'=>'http://www.esigaretervaring.nl/wp-content/uploads/2014/04/Elektrische-sigaret-voorbeeld-300x225.jpg',
+            'created_at' => $createdAt,
+            'updated_at' => $updatedAt
+        ]);
+
+        // Pages seeder
+        DB::table('pages')->insert([
+            'name' => 'Over',
+            'content' => 'Wij vinden het altijd leuk om reacties te krijgen. Onze voorkeur is per 
+        bezoek of e-mail, maar ook telefonisch, per fax of per post staan we je graag te 
+        woord. Gebruik hiervoor onderstaande contactinformatie: <br /> <br /> eSigarett.nl <br />',
+            'created_at' => $createdAt,
+            'updated_at' => $updatedAt
+        ]);
+
+        DB::table('pages')->insert([
+            'name' => 'Producten',
+            'content' => 'Productenlijst',
+            'created_at' => $createdAt,
+            'updated_at' => $updatedAt
+        ]);
+
+        DB::table('pages')->insert([
+            'name' => 'Contact',
+            'content' => 'Contact met ons',
             'created_at' => $createdAt,
             'updated_at' => $updatedAt
         ]);
--- resources/views/main/page.blade.php
+++ resources/views/main/page.blade.php
@@ -19,7 +19,7 @@
         <div class="row">
             <div class="col-md-12">
                 <span>
-                    {{ $page->content }}
+{!! $page->content !!}
                 </span>
             </div>
         </div>
