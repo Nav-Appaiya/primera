97a2b5e3be2f90f54ea03019b3dd0978475ed071
Nav Appaiya <navarajh@gmail.com>
2016-07-28 16:16:10 +0200

    Breadcrumbs linking to right page

--- resources/views/products/detail.blade.php
+++ resources/views/products/detail.blade.php
@@ -11,8 +11,8 @@
     <div class="row">
         <div class="col-md-12">
             <ol class="breadcrumb">
-                <li class="breadcrumb-item"><a href="#">Home</a></li>
-                <li class="breadcrumb-item"><a href="#">{{ $product->categories()->first()->title }}</a></li>
+                <li class="breadcrumb-item"><a href="{{ URL::route('homepage') }}">Home</a></li>
+                <li class="breadcrumb-item"><a href="{{ URL::route('category_detail', $product->categories()->first()->id) }}">{{ $product->categories()->first()->title }}</a></li>
                 <li class="breadcrumb-item active">{{ strtolower($product->name) }}</li>
             </ol>
         </div>
