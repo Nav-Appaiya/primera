186312d2c2b8ede92ff6ea7fa1b8ce2015fa0038
Nav Appaiya <navarajh@gmail.com>
2016-08-02 20:06:14 +0200

    cate_id was niet teogevoegd aan migratie, error bij install fixed

--- database/migrations/2016_06_07_000222_create_table_categories.php
+++ database/migrations/2016_06_07_000222_create_table_categories.php
@@ -15,6 +15,7 @@ class CreateTableCategories extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->increments('id');
             $table->string('categoryID');
+            $table->string('cate_id');
             $table->string('title');
             $table->string('image');
             $table->timestamps();
