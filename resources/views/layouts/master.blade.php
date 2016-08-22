a70869de928ad238b7f2e6d19c297cdf6d7b5a3c
Antoine Croes <antoine@10.0.0.22>
2016-08-23 00:36:47 +0200

    ad

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,73 +3,137 @@
         <title>Primera e-Sigarets - @yield('titel')</title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-        <link href='http://fonts.googleapis.com/css?family=Lato:100,400,700,900' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="http://fontawesome.io/assets/font-awesome/css/font-awesome.css">
         <link rel="stylesheet" href="/css/all.css">
     </head>
     <body>
-    <div class="top">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-6" style="text-align: left">Op werkdagen besteld voor 22:00, morgen in huis. Gratis retourneren</div>
-                <div class="col-md-6" style="text-align: right">Inloggen &nbsp; &nbsp; &nbsp; &nbsp; Nieuwe klant</div>
+        
+<div class="top">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 col-sm-6 align-left">
+                        Op werkdagen voor 22:00 uur besteld, morgen in huis.
+                    </div>
+                    <div class="col-md-6 col-sm-6 align-right">
+                        <a class="account-inf" data-toggle="modal" data-target="#myModal" href="">Mijn Account</a>
+                        <a class="account-inf" href="">Nog geen klant?</a>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
-    <header>
-        <div class="container">
-        <div class="row">
-            <div class="header-logo col-xs-12 col-md-4">
-            <input type="text" placeholder="Zoeken" style="float: left"><i  style="float: left; margin-top: 17px; margin-left: -25px;" class="glyphicon glyphicon-search"></i>
-            </div>
-            <div class="col-xs-12 col-md-4" style="text-align: center">
-                                <img src="http://esiggie.nl/wp-content/uploads/2014/12/Esiggie-logo.png" height="50px">
-            </div>
-            <div class="col-md-4">
-                        @if(Auth::check() == false)
-            <div class="shopping-cart blue">
-                Winkelwagen
-                    <span class="number">{{ count(Session::get('cart.items')) }}</span>
+        <header>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4 col-sm-4 col-xs-2">
+                        <i style="font-size: 25px; margin-top: 17.5px" class="fa fa-search" aria-hidden="true"></i>
+                    </div>
+                    <div class="col-md-4 col-sm-4 col-xs-8 align-center">
+        <img src="http://esigaret24.nl/wp-content/uploads/2015/11/damp-e-sigaret-webshop.jpg">
+                    </div>
+                    <div class="col-md-4 col-sm-4 col-xs-2">
+  @if(Auth::check() == false)
+                        <div style="float: right;">
+                            <i style="font-size: 25px; margin-top: 17.5px" class="fa fa-shopping-bag" aria-hidden="true"></i><span style="margin-top: -20px; margin-left: -8px;" class="badge">{{ count(Session::get('cart.items')) }}</span>
+                        </div>
+                        @else
+                        <div style="float: right;">
+                            <i style="font-size: 25px; margin-top: 17.5px" class="fa fa-shopping-bag" aria-hidden="true"></i><span style="margin-top: -20px; margin-left: -8px;" class="badge">{{ count(Session::get('cart.items')) }}</span>
+                        </div>
+                        
                         @endif
+                    </div>
+                </div>
             </div>
+        </header>  
+        <div class="navigation">
+            <div class="container">
+                <ul>
+                    <li>Homepagina</li>
+                    <li>categorieën</li>
+                    <li>Over ons</li>
+                    <li>Contact</li>
+                </ul>
             </div>
-        </div>
-        </div>
-    </header>
-        <div class="navigation"></div>
+        </div>    
 
                     @show
-                    <div class="container">
+                    <div class="container" style="margin-top: 40px">
+                        @yield('content')
                     </div>
-                    <footer class="footer">
-                        <div class="container">
-                            <span class="text-muted pull-left">
-                                Esigarett.nl - {{ date('Y') }}
-                            </span>
-                            <p class="text-muted pull-right">
-                                <img src="{{ asset('/assets/img/NIX18-cmyk-payoff-fc.png') }}" alt="nix18" width="75em">
-                            </p>
+<div class="container">
+    
+</div>
+
+<div id="myModal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-body">
+      <h2 class="align-center" style="margin-top: 20px">Mijn account</h2>
+<form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
+                        {{ csrf_field() }}
+
+                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}">
+
+                                @if ($errors->has('email'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
                         </div>
-                    </footer>
-                    <style>
-                    html {
-                    position: relative;
-                    min-height: 100%;
-                    }
-                    body {
-                    margin-bottom: 60px;
-                    }
-                    .footer {
-                    position: absolute;
-                    bottom: 0;
-                    width: 100%;
-                    height: 60px;
-                    background-color: #f5f5f5;
-                    }
-                    footer .text-muted {
-                    margin: 20px 0;
-                    }
-                    </style>
+
+                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                            <label for="password" class="col-md-4 control-label">Password</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control" name="password">
+
+                                @if ($errors->has('password'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <div class="checkbox">
+                                    <label>
+                                        <input type="checkbox" name="remember"> Remember Me
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary">
+                                    <i class="fa fa-btn fa-sign-in"></i> Login
+                                </button>
+                                <a class="btn btn-primary" href="/redirect">
+
+                                        <i class="fa fa-btn fa-sign-in"></i>
+                                    Facebook Login
+
+                                </a>
+
+                                <a class="btn btn-link" href="{{ url('/password/reset') }}">Forgot Your Password?</a>
+                            </div>
+                        </div>
+                    </form>
+      </div>
+    </div>
+
+  </div>
+</div>
+
                     <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
                     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
                     <script src="{{ URL::asset('assets/js/script.js') }}"></script>
--- resources/views/main/index.blade.php
+++ resources/views/main/index.blade.php
@@ -10,83 +10,11 @@
 @endsection
 
 @section('content')
-    <div class="container">
 
         <div class="row">
-            <div id="myCarousel" class="carousel slide" data-ride="carousel">
-                <!-- Wrapper for slides -->
-                <div class="carousel-inner">
-
-                    <div class="item active" style="max-height: 400px; height: 100%;">
-                        <img src="{{ asset('/uploads/img/esigaret5.jpg') }}">
-                        <div class="carousel-caption">
-                            <h2><span style="background-color: #2980b9;">Welkom bij esigarett.nl!</span></h2>
-                            <p><a href="/producten" class="btn btn-success">Bekijk onze producten</a></p>
-                        </div>
-                    </div><!-- End Item -->
-
-                    <div class="item" style="max-height: 400px; height: 100%;">
-                        <img src="{{ asset('/uploads/img/esigaret6.jpg') }}">
-                        <div class="carousel-caption">
-                            <h2><span style="background-color: #2980b9;">OOOOnze Producten</span></h2>
-                            <p>Een elektronische sigaret is een alternatief voor traditioneel roken. Er zijn e-sigaretten* met en zonder nicotine. De e-sigaret bevat een vloeibare vulling die wordt verdampt door middel van opwarming. De vloeistof wordt opgewarmd door een verwarmingselement met behulp van een batterij.
-
-                            </p>
-                        </div>
-                    </div><!-- End Item -->
-
-                    <div class="item" style="max-height: 400px; height: 100%;">
-                        <img src="{{ asset('/uploads/img/esigaret7.jpg') }}">
-                        <div class="carousel-caption">
-                            <h2><span style="background-color: #2980b9;">Onze bezorgdienst</span></h2>
-                            <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. <a href="http://sevenx.de/demo/bootstrap-carousel/" target="_blank" class="label label-danger">Bootstrap 3 - Carousel Collection</a></p>
-                        </div>
-                    </div><!-- End Item -->
-
-                    <div class="item" style="max-height: 400px; height: 100%;">
-                        <img src="{{ asset('/uploads/img/esigaret5.jpg') }}">
-                        <div class="carousel-caption">
-                            <h2><span style="background-color: #2980b9;">Contact</span></h2>
-
-                            <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p>
-                        </div>
-                    </div><!-- End Item -->
-
-                </div><!-- End Carousel Inner -->
-
-
-                <ul class="nav nav-pills nav-justified">
-                    <li data-target="#myCarousel" data-slide-to="0" class="active"><a href="#">eSigarett.nl<small>Lees meer over ons!</small></a></li>
-                    <li data-target="#myCarousel" data-slide-to="1"><a href="#">Producten<small>Ruim assortiment van producten!</small></a></li>
-                    <li data-target="#myCarousel" data-slide-to="2"><a href="#">Bezorgen<small>Binnen 3 dagen thuisbezorgd!</small></a></li>
-                    <li data-target="#myCarousel" data-slide-to="3"><a href="#">Contact<small>Natuurlijk kunt u ons ook bezoeken!</small></a></li>
-                </ul>
-
-
-            </div><!-- End Carousel -->
-
-            <style>
-                body { padding-top: 20px; }
-                #myCarousel .nav a small {
-                    display:block;
-                }
-                #myCarousel .nav {
-                    background:#eee;
-                }
-                #myCarousel .nav a {
-                    border-radius:0px;
-                }
-            </style>
-        </div>
-
-        <br>
-        <br>
-
-        <div class="row">
-            <div class="col-md-12">
             @foreach ($products as $product)
                     <div class="col-sm-6 col-md-4">
-                        <div class="thumbnail" >
+                        <div class="boxpanel">
                             <img src="{{$product->imageurl}}" class="img-responsive">
                             <div class="caption">
                                 <div class="row">
@@ -115,9 +43,7 @@
                     </div>
 
                 @endforeach
-            </div>
         </div>
-    </div>
 
 @endsection
 
