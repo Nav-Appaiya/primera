2392dc526b2063df90a301c218fc8bed94b11339
Antoine Croes <abcroes@192.168.2.5>
2016-11-13 00:58:36 +0100

    added

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -270,6 +270,9 @@
                                     @endif
                                 </div>
                             </div>
+                            <div class="information">
+                                <span class="product-hover">Bekijken</span>
+                            </div>
                         </div>
                     </div>
                 </a>
